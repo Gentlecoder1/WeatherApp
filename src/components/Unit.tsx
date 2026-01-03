fb2841b2a986865aadd138567a0fa1059721df24
Gentlecoder1 <israeloloruntoba3@gmail.com>
2026-01-03 18:46:50 +0100

    feat: add clsx dependency and implement unit conversion data in Unit component

--- src/components/Unit.tsx
+++ src/components/Unit.tsx
@@ -1,40 +1,33 @@
 import React from 'react'
+import { unitTemp } from './index'
 
 const Unit = () => {
+
   return (
     <div className='w-[214px] bg-[#262540] border border-[#3C3B5E] py-[6px] px-[8px] rounded-[12px]'>
         <div className='text-[16px] font-500 py-[10px] px-[8px]'>Switch to Imperial</div>
+
+
         <div className='flex flex-col gap-[5px]'>
-            <div className='flex flex-col gap-[8px]'>
-                <h1 className='#ACACB7 text-[15px] font-500'>Temperature</h1>
 
-                <div className='text-[16px] font-500'>
-                    <p>Celsius (°C)</p>
-                    <p>Fahrenheit (°F)</p>
+            {unitTemp.map(({ id, title, units }) => (
+                <div 
+                    key={id}
+                    className='flex flex-col gap-[8px]'
+                >
+                    <h1 className='#ACACB7 text-[15px] font-500'>{title}</h1>
+                    {units.map(({ id, name }) => (
+                        <div 
+                            key={id}
+                            className='text-[16px] font-500'
+                        >
+                            <p>{name}</p>
+                        </div>
+                    ))}
                 </div>
-            </div>
+            ))}
 
             <hr />
-
-            <div className='flex flex-col gap-[8px]'>
-                <h1 className='#ACACB7 text-[15px] font-500'>Wind Speed</h1>
-
-                <div className='text-[16px] font-500'>
-                    <p>km/h</p>
-                    <p>mph</p>
-                </div>
-            </div>
-
-            <hr />
-            
-            <div className='flex flex-col gap-[8px]'>
-                <h1 className='#ACACB7 text-[15px] font-500'>Precipitation</h1>
-
-                <div className='text-[16px] font-500'>
-                    <p>Millimeters (mm)</p>
-                    <p>Inches (in)</p>
-                </div>
-            </div>
         </div>
     </div>
   )
---
+++ src/components/index.ts
@@ -0,0 +1,26 @@
+export const unitTemp = [
+    {
+        id: 1,
+        title: "Temperature",
+        units: [
+            { id: 1, name: 'Celsius (°C)' },
+            { id: 2, name: 'Fahrenheit (°F)' }
+        ]
+    },
+    {
+        id: 1,
+        title: "Wind Speed",
+        units: [
+            { id: 1, name: 'km/h' },
+            { id: 2, name: 'mph' }
+        ]
+    },
+    {
+        id: 1,
+        title: "Precipitation",
+        units: [
+            { id: 1, name: 'Millimeters (mm)' },
+            { id: 2, name: 'Inches (in)' }
+        ]
+    }
+]
